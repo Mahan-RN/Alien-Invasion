0d8219d8f0d096d35d5627d32e4cd375fb68dd8a
Mahan <mahanrn@student.ubc.ca>
2025-06-16 22:11:05 -0700

    Adjusted spaceship speed settings for finer movement. Resolved circular import issue in the spaceship class

--- src/settings.py
+++ src/settings.py
@@ -8,3 +8,4 @@ class Settings:
         self.WIDTH: Final[int] = 1200
         self.HEIGHT: Final[int] = 800
         self.BACKGROUND_COLOR: Final[tuple[int, int, int]] = (230, 230, 230)
+        self.ship_speed: Final[float] = 6.5
--- src/space_ship.py
+++ src/space_ship.py
@@ -1,12 +1,14 @@
 import pygame
 
-from alien_invasion import AlienInvasion
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
 
 class Ship:
     """A class to manage the ship"""
 
-    def __init__(self, ai_game: AlienInvasion) -> None:
+    def __init__(self, ai_game: 'AlienInvasion') -> None:
         """Initialize the ship and set its starting position
 
         Args:
@@ -14,6 +16,7 @@ class Ship:
         """
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect
         self.image = pygame.image.load("src/resources/images/space_ship_image.png")
@@ -21,6 +24,9 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a float for the ship's exact horizontal position
+        self.x = float(self.rect.x)
         # Movement flag; start with a ship that's not moving
         self.moving_right: bool = False
         self.moving_left: bool = False
@@ -34,6 +40,9 @@ class Ship:
         """Update the ship's position based on the movement flag
         """        
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        # Update the rect object from the self.x
+        self.rect.x = int(self.x)
